79c301be316f258f9d98c4a0f4ef82e61e11ed48
elysiacasandra <egia0002@student.monash.edu>
2023-06-11 17:03:27 +1000

    DS

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/elysiagiannis/Documents/Weddings/weddingwebsite/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Abhaya_Libre\",\"arguments\":[{\"subsets\":[\"sinhala\"],\"weight\":\"400\"}],\"variableName\":\"inter\"}": [
+      "static/media/9b174b79d9b4926c-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
